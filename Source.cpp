6d1e7f37967d0017c1e04bad2faa4f1cabe9b87d
AsparaGus116 <gusbres@gmail.com>
2023-03-13 01:37:33 -0400

    Added main and test file

---
+++ Source.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <regex>
+#include <sstream>
+#include <conio.h>
+
+//TODO: Find way to get Windows / POSIX in #DEFINE statement
+
+extern const char* operators[] = { "()", "==", "=", "+", "-", "*", "/", "||", "&&", };
+
+void removeWhitespace(std::string& input)
+{
+	std::regex whitespace("[\\s]*");
+	input = std::regex_replace(input, whitespace, "");
+}
+
+void quit()
+{
+	std::cout << "Press any key to exit...";
+	while(!_kbhit() ){} // Windows only, find POSIX solution
+}
+
+int main(int argc, char* argv[])
+{
+	std::ifstream file{};
+	file.open("test.cbr", std::ios_base::in);
+
+	std::string in;
+	assert(file.good());
+	while (!file.eof())
+	{
+		in.push_back(file.get());
+	}
+	file.close();
+	removeWhitespace(in);
+	std::stringstream f{ in };
+
+	std::string line;
+	std::getline(f, line, ';');
+	std::cout << line;
+
+	quit();
+}
