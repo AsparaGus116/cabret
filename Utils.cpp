1ff4ad8b4c0f5935bb99bd7cd1596f9042e9ac39
AsparaGus116 <gusbres@gmail.com>
2023-03-14 00:12:32 -0400

    Pushed definitions into utils.cpp

--- Utils.cpp
+++ Utils.cpp
@@ -1,1 +1,17 @@
 #include "Utils.h"
+
+void utils::removeWhitespace(std::string& input)
+{
+	std::regex whitespace("[\\s]*");
+	input = std::regex_replace(input, whitespace, "");
+}
+
+void utils::quit()
+{
+#ifdef DEBUG
+	std::cout << "Press any key to exit...";
+	while (!_kbhit()) {} // Windows only, find POSIX solution
+#endif
+
+	std::exit(0);
+}
--- Utils.h
+++ Utils.h
@@ -5,20 +5,9 @@
 
 namespace utils
 {
-	void removeWhitespace(std::string& input)
-	{
-		std::regex whitespace("[\\s]*");
-		input = std::regex_replace(input, whitespace, "");
-	}
+	void removeWhitespace(std::string& input);
 
-	void quit()
-	{
-#ifdef DEBUG
-		std::cout << "Press any key to exit...";
-		while (!_kbhit()) {} // Windows only, find POSIX solution
-#endif
-
-		std::exit(0);
-	}
+	void quit();
+	
 }
 
