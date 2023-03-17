d7e88638c2747a6f674ae531606e14f11f9edd6b
August Bresnaider <gusbres@gmail.com>
2023-03-17 12:36:29 -0400

    Added TODO comments

--- Utils.cpp
+++ Utils.cpp
@@ -15,7 +15,9 @@ void utils::preProcess(std::string& input)
 	std::regex comments("(\\/\\/)(.)+\\n");
 
 	input = std::regex_replace(input, comments, "\n");
-	input = std::regex_replace(input, define, "#$1;");
+	input = std::regex_replace(input, define, "#$1;"); 
+	// TODO: Change define and undefine characters (extended char set?), maybe set up an enum
+	// TODO: Create separate preprocessor class
 	input = std::regex_replace(input, undef, "^$1;");
 	input = std::regex_replace(input, whitespace, "");
 	
