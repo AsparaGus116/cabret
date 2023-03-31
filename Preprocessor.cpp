f9e2494588f201a430e38e4d3f956348e99e9b4c
August Bresnaider <gusbres@gmail.com>
2023-03-31 12:12:44 -0400

    Added multi-line comments (Might need to rework preprocessor to not remove spaces

--- Preprocessor.cpp
+++ Preprocessor.cpp
@@ -10,7 +10,9 @@ void Preprocessor::preprocess(std::string& buffer)
 	std::regex undef("#undef\\s+(\\w+)\n?", icase);
 	std::regex whitespace("[\\s]*");
 	std::regex comments("(\\/\\/)(.)+\\n");
+	std::regex multiline_comments("\\/\\*((.)|(\\n))*?(\\*\\/)");
 
+	buffer = std::regex_replace(buffer, multiline_comments, "");
 	buffer = std::regex_replace(buffer, comments, "\n");
 	buffer = std::regex_replace(buffer, define, def_delim + "$1;");
 	// TODO: Change define and undefine characters (extended char set?), maybe set up an enum
