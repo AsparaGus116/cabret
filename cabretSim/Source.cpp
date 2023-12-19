a264f04200bbb99de812cde4c07e1c71c2bc3e25
August Bresnaider <gusbres@gmail.com>
2023-12-18 23:36:22 -0500

    Added file I/O

--- cabretSim/Source.cpp
+++ cabretSim/Source.cpp
@@ -1,10 +1,29 @@
 #include <array>
 #include <iostream>
+#include <fstream>
 std::array<uint8_t, 4> R; // Registers
 std::array<uint8_t, 256> M; // Memory array
 
 int pc; // Program counter
 int main(int argc, char* argv[])
 {
+	std::ifstream file;
+	if (argc >= 2) // File input
+	{
+		file.open(argv[1], std::ios::in);
+	}
+	else
+	{
+		std::string filename;
+		std::cout << "Enter filename: " << "\n>> ";
+		std::cin >> filename;
+		file.open(filename);
+	}
+
+	if (!file.good())
+	{
+		std::cout << "ERR: Invalid file\n";
+		return -1;
+	}
 	
 }
