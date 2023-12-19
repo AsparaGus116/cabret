cc2a6fa96afc4cac686432f9f4af033d3645be34
August Bresnaider <gusbres@gmail.com>
2023-12-18 23:39:15 -0500

    Added opcode functionality

--- cabretSim/Source.cpp
+++ cabretSim/Source.cpp
@@ -39,5 +39,105 @@ int main(int argc, char* argv[])
 	}
 	
 	pc = 0;
+	while (1)
+	{
+		std::cout << "How many steps would you like to go? (-1 to run until halt) \n>> ";
+		int x;
+		std::cin >> x;
+
+		for (int z = 0; z < (uint32_t)x; z++) // if x == -1, run a VERY long number of steps (usually until halt or end of memory)
+		{
+			int opcode = utils::getBits(M[pc], 4, 7);
+			int a = utils::getBits(M[pc], 2, 4);
+			int b = utils::getBits(M[pc], 0, 2);
+
+			if (pc > 0xff || (pc == 0xff && opcode == 6))
+			{
+				return 0;
+			}
+
+			if (M[pc] & 0x80) // reserved bit is set
+			{
+				// Halt
+				return 0;
+			}
+
+			switch (opcode)
+			{
+			case 0:
+				R[a] = R[b];
+				++pc;
+				break;
+			case 1:
+				R[a] += R[b];
+				++pc;
+				break;
+			case 2:
+				R[a] &= R[b];
+				++pc;
+				break;
+			case 3:
+				R[a] = M[R[b]];
+				++pc;
+				break;
+			case 4:
+				M[R[b]] = R[a];
+				++pc;
+				break;
+			case 5:
+				switch (b)
+				{
+				case 0:
+					R[a] = ~R[a];
+					++pc;
+					break;
+				case 1:
+					R[a] = -R[a];
+					++pc;
+					break;
+				case 2:
+					R[a] = !R[a];
+					++pc;
+					break;
+				case 3:
+					R[a] = pc;
+					++pc;
+					break;
+				}
+				break;
+			case 6:
+				switch (b)
+				{
+				case 0:
+					R[a] = M[pc + 1];
+					pc += 2;
+					break;
+				case 1:
+					R[a] += M[pc + 1];
+					pc += 2;
+					break;
+				case 2:
+					R[a] &= M[pc + 1];
+					pc += 2;
+					break;
+				case 3:
+					R[a] = M[M[pc + 1]];
+					pc += 2;
+					break;
+				}
+				break;
+			case 7:
+				if (R[a] & 0x80 || R[a] == 0) // LE 0
+				{
+					pc = R[b];
+				}
+				else
+				{
+					++pc;
+				}
+				break;
+			}
+		}
+	}
 	
 }
